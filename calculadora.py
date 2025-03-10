1207cb08c5dc3e65746b513461a581135beb91f6
Pedro-luiz-melo <pedro.venancio@pdbomdespacho.com.br>
2025-03-10 16:52:58 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+nl = tloat (input())
+operador = input()
+n2 = float (input())
+if operador == "+":
+resultado = n1 + n2
+elif operador == "-":
+resultado = n1 - n2
+elif
+operador um "/":
+resultado = n1 / n2
+elif
+operador =="*":
+resultado=n1 * n2
+print (f" (n1) (operador) (n2)=(resultado}")
